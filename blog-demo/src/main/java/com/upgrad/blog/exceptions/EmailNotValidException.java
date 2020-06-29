67875c4f4015ca6e858b16e67bb71f3bb1fd653c
surya444 <gudusuryateja@gmail.com>
2020-06-29 12:39:55 +0530

    project

--- blog-demo/src/main/java/com/upgrad/blog/exceptions/EmailNotValidException.java
+++ blog-demo/src/main/java/com/upgrad/blog/exceptions/EmailNotValidException.java
@@ -5,41 +5,21 @@ package com.upgrad.blog.exceptions;
  * TODO: 3.2. Provide a constructor which accepts a custom message as input
  * parameter and passes it to its base class.
  */
-public class EmailNotValidException extends Exception{
+public class EmailNotValidException {
 	//*****************START*****************Solution 3.1 + 3.2*****************
-
-    public EmailNotValidException() {
-    }
-
-    public EmailNotValidException(String message) {
-        super(message);
-    }
-
-    public EmailNotValidException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public EmailNotValidException(Throwable cause) {
-        super(cause);
-    }
-
-    public EmailNotValidException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
 
     //*****************END*****************Solution 3.1 + 3.2*****************
     
-    public static void main(String[] args) {
-		try {
-			throw new EmailNotValidException("Custom Message");
-		} catch (EmailNotValidException e) {
-			System.out.println(e.getMessage());
-		}
-
-		/**
-		 * Your output should look like this.
-		 * Custom Message
-		 */
-	}
+//    public static void main(String[] args) {
+//		try {
+//			throw new EmailNotValidException("Custom Message");
+//		} catch (EmailNotValidException e) {
+//			System.out.println(e.getMessage());
+//		}
+//		
+//		/**
+//		 * Your output should look like this.
+//		 * Custom Message
+//		 */
+//	}
 }
